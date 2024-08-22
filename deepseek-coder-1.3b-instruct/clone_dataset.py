77aae0ab5ff8642546e17699af8b7adbdc9183ae
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-08-22 10:29:09 +0500

    Deleted deepseek-coder-1.3b-instruct folder, added Llama dir

--- deepseek-coder-1.3b-instruct/clone_dataset.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import login
-from huggingface_hub import Repository
-
-login("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",add_to_git_credential=True)
-
-repo_url='https://huggingface.co/datasets/CodexAI/Test4Deepseek-coder'
-
-repo = Repository(local_dir="data", clone_from=repo_url)
