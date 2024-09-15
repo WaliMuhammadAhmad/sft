90e8dc0e557a4d214a8e2e8f42c08cc04db57cf1
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-09-16 00:04:54 +0500

    junk removed

--- clone_repo.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import login
-from huggingface_hub import Repository
-
-login("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",add_to_git_credential=True)
-
-repo_id='CodexAI/Trainllama'
-repo_url='https://huggingface.co/datasets/CodexAI/Train4llama'
-
-repo = Repository(local_dir="eval", clone_from=repo_url)
